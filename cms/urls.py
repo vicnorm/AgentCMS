3324832b9d1ae7d8dc45c8cc6dca8f819dac3716
vicnorm <112108962+vicnorm@users.noreply.github.com>
2026-03-08 16:18:28 +0100

    Polish homepage layout and styling

--- cms/urls.py
+++ cms/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import index, page_detail
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("<slug:slug>/", page_detail, name="page_detail"),
+    path('', index, name='index'),
+    path('home/', index, name='home'),
+    path('<slug:slug>/', page_detail, name='page_detail'),
 ]
